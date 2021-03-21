2d76abb0f64af30821734e86e6cf696d230102f1
Sushant Kakroo <sushant.kakroo@lybrate.com>
2021-03-21 18:37:16 +0530

    paintgit : functions ;

--- src/server.js
+++ src/server.js
@@ -4,35 +4,37 @@ const path = require('path');
 const app = express();
 const serverless = require('serverless-http');
 
-app.set('views', path.join(__dirname, 'views'));
-app.set('view engine', 'ejs');
+// app.set('views', path.join(__dirname, 'views'));
+// app.set('view engine', 'ejs');
 
-app.use(function(req, res, next) {
-	console.log("request intercepted : ", req.method, " ", req.path);
-	next();
-});
+// app.use(function(req, res, next) {
+// 	console.log("request intercepted : ", req.method, " ", req.path);
+// 	next();
+// });
 
-app.use('/static', express.static(path.join(__dirname, './public')));
+// app.use('/static', express.static(path.join(__dirname, './public')));
 
-app.get(".netlify/functions/test", function(req, res, next) {
+const router = express.Router();
+router.get("/test", function(req, res, next) {
 	res.json({
 		"hello": "world"
 	});
 });
 
-app.get(".netlify/functions/index", function(req, res, next) {
-	res.render("index", {title: "Home Page"});
-});
+// router.get("/index", function(req, res, next) {
+// 	res.render("index", {title: "Home Page"});
+// });
 
-app.use(clientErrorHandler);
-function clientErrorHandler (err, req, res, next) {
-	console.log("error : ", err);
-	if (req.xhr) {
-		console.log("unknown api : ");
-	} else {
-		console.log("incorrect path configured");
-	}
-	next();
-}
+app.use('.netlify/functions/view', router);
+// app.use(clientErrorHandler);
+// function clientErrorHandler (err, req, res, next) {
+// 	console.log("error : ", err);
+// 	if (req.xhr) {
+// 		console.log("unknown api : ");
+// 	} else {
+// 		console.log("incorrect path configured");
+// 	}
+// 	next();
+// }
 
 module.exports.handler = serverless(app);
